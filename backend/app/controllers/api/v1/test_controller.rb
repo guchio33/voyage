ab23021b4b806e61bed2f6faeb87445d5a0ea1cc
Shoki-popo-git <popotangr@gmail.com>
2021-10-28 19:08:06 +0900

    railsとreactを繋げる

--- backend/app/controllers/api/v1/test_controller.rb
+++
@@ -1,2 +0,0 @@
-class Api::V1::TestController < ApplicationController
-end
--- backend/app/controllers/application_controller.rb
+++
@@ -1,5 +0,0 @@
-class ApplicationController < ActionController::API
-    def index
-        render json: { status: 200, message: "Hello World!"}
-    end
-end
