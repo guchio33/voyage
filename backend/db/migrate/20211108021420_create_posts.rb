797f40de7c24c10a0f8048972c5b7e738d68ea1d
Shoki-popo-git <popotangr@gmail.com>
2021-11-10 22:50:01 +0900

    postテーブルのカラムを変更

--- backend/db/migrate/20211108021420_create_posts.rb
+++ backend/db/migrate/20211108021420_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.string :image
-      t.string :place_name
+      t.string :photo
+      t.string :place
       t.string :prefecture
       t.string :genre
 
