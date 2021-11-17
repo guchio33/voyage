9dd9cd15b424107e49ff6011a6b2d3f49864a458
Shoki-popo-git <popotangr@gmail.com>
2021-11-17 18:49:37 +0900

    ログイン情報をrailsで受け取る

---
+++ backend/app/controllers/auth/registrations_controller.rb
@@ -0,0 +1,8 @@
+# アカウント作成用コントローラー
+class Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
+    private
+  
+      def sign_up_params
+        params.permit(:email, :password, :password_confirmation, :name)
+      end
+end
