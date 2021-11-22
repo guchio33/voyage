091b6eb2799f8fc743240432d9d13a3e71563dfe
moyarohi <mottizu@gmail.com>
2021-11-22 20:34:59 +0900

    タグ追加

--- frontend/src/component/NewPost.tsx
+++ frontend/src/component/NewPost.tsx
@@ -8,10 +8,11 @@ export const NewPost: VFC = memo(() => {
   const [genre, setGenre] = useState('');
   const [image, setImage] = useState<File | null>(null)
   const [place, setPlace] = useState('');
+  const [tags, setTags] = useState(['']);
 
   const history = useNavigate()
 
-  
+
   const selectImage = (e: any) => {
     const selectedImage = e.target.files[0]
     setImage(selectedImage)
@@ -24,7 +25,8 @@ export const NewPost: VFC = memo(() => {
     formData.append('post[place]', place)
     formData.append('post[prefecture]', prefecture)
     formData.append('post[genre]', genre)
-    
+    formData.append('post[tag]', tags.join(','))
+
     return formData
   }
 
@@ -32,23 +34,48 @@ export const NewPost: VFC = memo(() => {
     const url = 'http://localhost:3001/posts'
     const data = createFormData()
     axios.post(url, data)
-    .then(() => history('/allpost'))
-    .catch(e => {
-      console.error(e)
-    })
+      .then(() => history('/allpost'))
+      .catch(e => {
+        console.error(e)
+      })
+  }
+
+  //タグの数を変える関数
+  const addTag = () => {
+    let tags2 = [...tags]
+    tags2.push('')
+    setTags(tags2)
+  }
+  const deleteTag = () => {
+    let tags2 = [...tags]
+    tags2.pop()
+    setTags(tags2)
   }
 
   return (
     <>
-        <div>NEW POST</div>
-        <p>場所：<input type="text" value={place} onChange={(e) => setPlace(e.target.value)} /></p>
+      <div>NEW POST</div>
+      <p>場所：<input type="text" value={place} onChange={(e) => setPlace(e.target.value)} /></p>
 
-        <p>都道府県：<input type="text" value={prefecture} onChange={(e) => setPrefecture(e.target.value)} /></p>
+      <p>都道府県：<input type="text" value={prefecture} onChange={(e) => setPrefecture(e.target.value)} /></p>
 
-        <p>ジャンル：<input type="text" value={genre} onChange={(e) => setGenre(e.target.value)} /></p>
-        <div>File</div>
-        <input type="file" onChange={(e) => selectImage(e)} />
-        <button onClick={sendFormData}  color="white" >Create</button>
+      <p>ジャンル：<input type="text" value={genre} onChange={(e) => setGenre(e.target.value)} /></p>
+
+      <p>タグ：</p>
+      {tags.map((tag, index) =>
+        <p key={index}><input type="text" value={tag} onChange={(e) => {
+          let tags2 = [...tags]
+          tags2[index] = e.target.value;
+          setTags(tags2)
+        }} />
+        </p>
+      )}
+
+      <p><button onClick={addTag}>タグ追加</button><button onClick={deleteTag}>タグ削除</button></p>
+
+      <div>File</div>
+      <input type="file" onChange={(e) => selectImage(e)} />
+      <button onClick={sendFormData} color="white" >Create</button>
     </>
   )
 })
